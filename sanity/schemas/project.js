19fe88562e30a37b92d2726544b6b7367dd31f1e
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2022-11-29 23:16:24 +0100

    added contant to sanity, added dateformatter, urldataimage, got sanity working for projects

--- sanity/schemas/project.js
+++ sanity/schemas/project.js
@@ -18,5 +18,44 @@ export default {
         maxLength: 96,
       },
     },
+
+    {
+      name: 'date',
+      title: 'Date project creation',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY MMMM',
+      },
+    },
+    {
+      name: 'backgroundimage',
+      title: 'Background image',
+      type: 'image',
+      fields: [
+        {
+          name: 'alttext',
+          title: 'Alt text',
+          type: 'string',
+        },
+      ],
+    },
+
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: { type: 'tag' },
+        },
+      ],
+    },
   ],
 };
--- src/sanity/api.js
+++ src/sanity/api.js
@@ -1,14 +1,21 @@
 import { useSanityClient } from 'astro-sanity';
 import { createImageBuilder } from 'astro-sanity';
+import { parseISO, format } from 'date-fns';
+
+const imageBuilder = createImageBuilder(useSanityClient());
 
 export async function getFirstProject() {
-  const query = `*[_type == "post"] { tag-> }`;
+  const query = `*[_type == "project"]{..., backgroundimage{alttext, asset->}, "tags": tags[]->name}`;
   const firstProject = await useSanityClient().fetch(query);
   return firstProject;
 }
 
-export const imageBuilder = createImageBuilder(useSanityClient());
-
 export function urlForImage(source) {
   return imageBuilder.image(source);
+}
+
+export function formatDate(date) {
+  const dateString = parseISO(date);
+  const formattedDateString = format(dateString, 'MMMM yyyy');
+  return `${formattedDateString}`;
 }

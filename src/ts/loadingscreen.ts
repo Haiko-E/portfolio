981f44a52a7ec1dc8a6fa195aae898c7ee379b01
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2022-12-04 23:31:31 +0100

    add loadingscreen

---
+++ src/ts/loadingscreen.ts
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+  fadeOutLoadingScreen();
+});
+
+function fadeOutLoadingScreen() {
+  const loader = document.getElementById('loading-screen');
+  const logo = document.getElementById('logo');
+
+  const fadeOutDuration = 2600;
+  const fadeOutLogoDuration = 2400;
+
+  loader?.classList.add('add-animation');
+  logo?.classList.add('fade-out');
+
+  setTimeout(function () {
+    logo?.classList.add('hidden');
+  }, fadeOutLogoDuration);
+
+  setTimeout(function () {
+    console.log('set hidden to classlist');
+    loader?.classList.add('hidden');
+  }, fadeOutDuration - 700);
+}
+
+export {};
